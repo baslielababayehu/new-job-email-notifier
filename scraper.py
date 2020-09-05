c6c23b7a99b5f76f3b39c7f872cf3c2c54674710
Basliel <bababayehu22@wooster.edu>
2020-09-04 22:08:16 -0400

    stored results in an array

--- scraper.py
+++ scraper.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome("./chromedriver")
 
-desired_position = input('what job would you like search for?')
+# desired_position = input('what job would you like search for?')
+desired_position = "software engineering intern"
 
 new_desired_position = desired_position.split(" ")
 separator = "%20"
@@ -16,12 +17,13 @@ url_version_of_job = separator.join(new_desired_position)
 URL = f'https://www.indeed.ca/jobs?q={url_version_of_job}&l=vancouver'
 driver.get(URL)
 
+all_jobs = driver.find_elements_by_class_name('result')
+
 def scrape_indeed():
-
-  all_jobs = driver.find_elements_by_class_name('result')
-
   i = 1
+  array_of_all_jobs = []
   for job in all_jobs:
+    array_of_individual_jobs = []
     result_html = job.get_attribute('innerHTML')
     soup = BeautifulSoup(result_html, 'html.parser')
 
@@ -40,15 +42,57 @@ def scrape_indeed():
       location = "No Location Listed"
     
     # link = soup.find("a", class_="jobtitle").href
-    print(link)
+    # print(link)
+
+    job_summary_div = job.find_elements_by_class_name("summary")[0]
+
+    try:
+      job_summary_div.click()
+    except:
+      try:
+        close_popover = driver.find_elements_by_class_name("popover-x-button-close")
+        close_popover.click()
+        job_summary_div.click()
+      except:
+        print("do something else")
+    description = driver.find_element_by_id('auxCol')[1]
+    print(description)
+    # analyze_description()
 
     message = f"{i}. the position is listed as '{title}' and is located at {location}"
-    print(message)
+    # print(message)
+    array_of_individual_jobs.append(title)
+    array_of_individual_jobs.append(location)
+    array_of_all_jobs.append(array_of_individual_jobs)
+
+
     i = i + 1
+  # print(array_of_all_jobs)
   send_email()
 
 def send_email():
   print("EMAIL HAS BEEN SENT")
 
+
+def analyze_description():
+
+  for description in all_jobs:
+    result_html = description.get_attribute('innerHTML')
+    soup = BeautifulSoup(result_html, 'html.parser')
+    try:
+      job_description = soup.find(id="jobsearch-SerpJobCard").text
+    except:
+      job_description= "nothing found"
+  
+    print(job_description)
+    print("analyzing job description")
+  # use selenium to find job description
+
+  # search for a specified key word 
+
+  # record the number of times a word appears 
+
+  # Assign a score to the description 
 scrape_indeed()
 
+driver.close()
